eb7298adf30c81fce70b90adc687f070956d2236
lloydeguzman <118794978+lloydeguzman@users.noreply.github.com>
2023-02-22 19:32:42 +0800

    minor-v.1.3

---
+++ src/Data/sample.js
@@ -0,0 +1,23 @@
+export const USER_DATA = [
+    {
+        name: "Jim Lloyd De Guzman dddddddd gggggggg",
+        username: "jdeguzman0123",
+    },
+    {
+        name: "Krishna Fagara",
+        username: "kfagara0123",
+    },
+    {
+        name: "Christian Alday",
+        username: "calday0123",
+    },
+    {
+        name: "Marionne Quintanna",
+        username: "mquintanna0123",
+    },
+    {
+        name: "Jay-Ann Oliveros",
+        username: "joliveros0123",
+    },
+  ];
+  

31f4afa34cdddf6a49e306c476e35ac9daf3fbc1
Christian Aerone <christian.alday@gmail.com>
2023-02-26 21:06:38 +0800

    Profile Setup

---
+++ src/components/ProfilePostPage.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+import CommentIcon from "@mui/icons-material/Comment";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Avatar,
+  CardHeader,
+  IconButton,
+  CardActions,
+  Checkbox,
+  Menu,
+  MenuItem
+} from "@mui/material";
+import sana from "../images/sana.jpg";
+import "../style/Profile.css";
+import open from "../images/open.png";
+import close from "../images/close.png";
+function ProfilePostPage({ post }) {
+
+  const [like, setLike] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+
+  const handleChangeIcon = () => {
+    if (like === false) {
+      setLike(true);
+    } else {
+      setLike(false);
+    }
+  };
+
+  const menuId = "primary-search-account-menu";
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Edit</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Delete</MenuItem>
+    </Menu>
+  );
+
+  return (
+    <>
+      <Grid item xs={6} sx={{ backgroundColor: "#DFDEDB"}}></Grid>
+      <Grid item xs={8} sx={{ backgroundColor: "#DFDEDB" }}>
+        <Card sx={{ maxWidth: "50%" }} className="post">
+          <CardHeader
+            avatar={<Avatar src={sana} sx={{ bgcolor: "purple" }} />}
+            action={
+              <IconButton aria-label="settings" aria-haspopup="true" aria-controls={menuId} onClick={handleProfileMenuOpen}>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={post.name}
+            subheader={post.postDate}
+          />
+          <CardContent className="post-body">
+            <Typography>{post.postMessage}</Typography>
+            {}
+          </CardContent>
+          
+          <CardActions disableSpacing >
+            <IconButton className="likebtn" onClick={handleChangeIcon}>
+              {
+                like ? <img src={close} height="40px" /> : <img src={open} height="40px" />
+              }
+            </IconButton>
+            <IconButton aria-label="comment">
+              <CommentIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+        {renderMenu}
+      </Grid>
+      <Grid item xs={2} sx={{ backgroundColor: "#DFDEDB" }}>
+      </Grid>
+    </>
+  );
+}
+
+export default ProfilePostPage;
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,17 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import sana from "../images/sana.jpg";
+import { Grid, Avatar } from "@mui/material";
+import "../style/Profile.css";
+import NavBar from "../components/NavBar";
+import ProfilePostPage from "../components/ProfilePostPage";
 
-    
-const ProfilePage = () => {
-    const [form, setForm] = useState({})
-    const see = (formClone) =>{
-        setForm({
-            form,
-            formClone,
-        })
-    }
+const ProfilePage = ({ posts }) => {
+  // const [posts, setPosts] = useState([])
+  // const [followers, setFollowers] = useState([]);
+  // const [following, setFollowing] = useState([]);
+
+  // const getPosts = async () => {
+  //   const response = await fetch("../data/posts.js")
+  //   const data = await response.json();
+  //   setPosts(data);
+  // }
+  // const getFollowers = async () => {
+  //   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  //   const data = await response.json();
+  //   setFollowers(data);
+  // };
+  // const getFollowing = async () => {
+  //   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  //   const data = await response.json();
+  //   setFollowing(data);
+  // };
+
+  // useEffect(() => {
+  //   getPosts()
+  //   getFollowers();
+  //   getFollowing();
+  // }, []);
+
+  console.log("This is POST_DATA from ProfilePage.js", posts);
   return (
-    <img src={form.image}></img>
-  )
-}
+    <>
+      <NavBar />
+      <Grid
+        container
+        style={{ minHeight: "100vh" }}
+        spacing={3}
+        className="main-container"
+      >
+        <Grid item xs={5}>
+          <div className="profile-pic">
+            <Avatar src={sana} sx={{ width: 250, height: 250 }} />
+          </div>
+        </Grid>
+        <Grid item xs >
+          <div className="profile-name">Nikki Fagara</div>
+          <div className="profile-tag">@NikkiFagara</div>
+          <div className="profile-bio">
+            <em>"I am the greatest..."</em>
+          </div>
+          <Grid container spacing={2} columns={15} className="sub-container">
+            <Grid item xs={2.7} className="left">
+              <div className="left-title">Posts</div>
+              <div className="left-sub">12345</div>
+            </Grid>
+            <Grid item xs={4} className="middle">
+              <div className="middle-title">Followers</div>
+              <div className="middle-sub">5.6m</div>
+            </Grid>
+            <Grid item xs={2} className="right">
+              <div className="right-title">Following</div>
+              <div className="right-sub">3</div>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} columns={16} className="posts-container">
+          {posts.map((post) => (
+            <ProfilePostPage key={posts.id} post={post} />
+          ))}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;

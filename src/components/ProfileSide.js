402774fe4483ce8848642c7db82a9fe9a6edf739
Christian Aerone <christian.alday@gmail.com>
2023-02-26 21:02:05 +0800

    Feed Update from Nikki

--- src/components/ProfileSide.js
+++ src/components/ProfileSide.js
@@ -1,12 +1,49 @@
-import { Grid, Paper } from '@mui/material'
+import { Divider, Grid, Paper } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
+import dp from '../images/nik.jpg'
 
-const ProfileSide = () => {
+const ProfileSide = ({ theme }) => {
+
+//   const navigate = useNavigate();
+
+//   const GoToProfile = async (event) => {
+//     navigate("/profile/:username")
+// }
+
   return (
-    <div style={{ minWidth: "100%", marginTop: "5px"}}>
-    <Grid container sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <Paper sx={{width: "95%", height: "auto"}}>
-            Profile
+    <div className='profileSide' style={{ minWidth: "100%", marginTop: "10px"}}>
+    <Grid container sx={{display: "flex", justifyContent: "center", alignItems: "center"}} >
+        
+        <Paper className='profile' sx={{width: "90%", height: "300px", paddingBottom: "10px", borderRadius:"0.6rem", boxShadow:"2"}}>
+           <Grid item className='header' 
+              sx={{width: "100%", height: "90px", backgroundColor: "#FFE5D9", backgroundSize: "26%", backgroundAttachment:"fixed", paddingBottom: "15px", borderRadius:"0.6rem"}}/>
+           <Grid item className='profileDp' sx={{ width: "100%", marginTop:"-50px", justifyContent:"center", display: "flex" }}>
+              <img src={dp} alt=""/>
+          </Grid>
+           <Box className="names" sx={{ marginTop:"10px" }}>
+             <Box className="name">
+               <span>Nikki Fagara</span>
+             </Box>
+             <Box className="username">
+             <span>@nikkifagara</span>
+             </Box>
+           </Box>
+           <Divider variant="fullWidth" />
+           <div className='followerPart'>
+             <Box className='followers' sx={{ width: "100%", p: 2.5 }}>
+               <span>802</span>
+               <span>Followers</span>
+             </Box>
+             <Box className='following' sx={{ width: "100%", p: 2.5 }}>
+               <span>521</span>
+               <span>Following</span>
+             </Box>
+           </div>
+        </Paper>
+
+        <Paper className='latchList' sx={{width: "90%", height: "200px", paddingBottom: "10px", borderRadius:"0.6rem", boxShadow:"2", marginTop:"7px" }}>
+
         </Paper>
     </Grid>
     </div>
--- src/components/TrendSide.js
+++ src/components/TrendSide.js
@@ -1,12 +1,63 @@
-import { Grid, Paper } from '@mui/material'
+import { Box, Divider, Grid, Paper } from '@mui/material'
 import React from 'react'
+
 
 const TrendSide = () => {
   return (
-    <div style={{ minWidth: "100%", marginTop: "5px"}}>
-    <Grid container sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <Paper sx={{width: "95%", height: "auto"}}>
-            Trend
+    <div className='trendSide' style={{ minWidth: "100%", marginTop: "10px"}}>
+    <Grid container className='trend-part' sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <Paper className='trends' sx={{width: "90%", height: "350px", paddingBottom: "10px", borderRadius:"0.6rem", boxShadow:"2"}}>
+          <Grid item className='trendGrid' xs={12} sx={{ p: 2, display:"flex", justifyContent:"center", alignItems:"center" }}>
+            <span>Talks for you</span>
+          </Grid>
+          <Divider variant="fullWidth" />
+          <Grid item className='trendTopic' xs={12}>
+            <Box className="talks" >
+              {/* <Box className="rank" sx={{ width: "10%", p: 1}}>
+                <span>1</span>
+              </Box> */}
+              <Box className="talksTopic" sx={{ width: "90%", p: 0.5}}>
+                <span>Topic</span>
+                <span>talks</span>
+              </Box>
+            </Box>
+            <Box className="talks" >
+              {/* <Box className="rank" sx={{ width: "10%", p: 1}}>
+                <span>2</span>
+              </Box> */}
+              <Box className="talksTopic" sx={{ width: "90%", p: 0.5}}>
+                <span>Talks for you</span>
+                <span>talks</span>
+              </Box>
+            </Box>
+            <Box className="talks" >
+              {/* <Box className="rank" sx={{ width: "10%", p: 1}}>
+                <span>3</span>
+              </Box> */}
+              <Box className="talksTopic" sx={{ width: "90%", p: 0.5}}>
+                <span>Talks for you</span>
+                <span>talks</span>
+              </Box>
+            </Box>
+            <Box className="talks" >
+              {/* <Box className="rank" sx={{ width: "10%", p: 1}}>
+                <span>4</span>
+              </Box> */}
+              <Box className="talksTopic" sx={{ width: "90%", p: 0.5}}>
+                <span>Talks for you</span>
+                <span>talks</span>
+              </Box>
+            </Box>
+            <Box className="talks" >
+              {/* <Box className="rank" sx={{ width: "10%", p: 1}}>
+                <span>5</span>
+              </Box> */}
+              <Box className="talksTopic" sx={{ width: "90%", p: 0.5}}>
+                <span>Talks for you</span>
+                <span>talks</span>
+              </Box>
+            </Box>
+          </Grid>
         </Paper>
     </Grid>
     </div>
